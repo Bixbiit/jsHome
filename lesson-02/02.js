ec2b04a2ae47ecf3d4781824bc725c5c2a19e412
Bixbiit <nikfaircopy@gmail.com>
2025-07-23 17:44:02 +0300

    lesson-02-02

--- lesson-02/02.js
+++ lesson-02/02.js
@@ -7,9 +7,18 @@
 * Для успешного прохождения тестов не меняйте названия переменных!
 */
 
-let dice1 // your code
-let dice2 // your code
-let isWinningDouble // your code
+let dice1 = Math.floor(Math.random() * 6 + 1);
+let dice2 = Math.floor(Math.random() * 6 + 1);
+let isWinningDouble = false
+while(!isWinningDouble){
+    if((dice1 === dice2) && (dice1 >3)){
+        console.log(`Pobeda : ${dice1} i ${dice2} `)
+    isWinningDouble = true
+}else{
+    console.log(`Proigral: ${dice1} i ${dice2} `)
+    isWinningDouble =  true
+}
+}
 
 /*
 console.log('Первый бросок: ' + dice1)
